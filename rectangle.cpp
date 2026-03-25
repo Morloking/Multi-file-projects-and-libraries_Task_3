8d6a10dbdc0f4d2646aad2a2ae18120bbbfb85c5
Morloking <dyubenkosergei@yandex.ru>
2026-03-25 19:52:48 +0300

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,5 +1,5 @@
 #include "rectangle.h"
 
 my_namespace::Rectangle::Rectangle(int l1, int l2) :
-	Quadrangle(l1, l2, l1, l2, 90, 90, 90, 90, "Прямоугольник") {}
+	Quadrangle(l1, l2, l1, l2, 90, 90, 90, 90, "РџСЂСЏРјРѕСѓРіРѕР»СЊРЅРёРє") {}
 

45dd79e2535b32de56712ac2fc38058f4c63f355
Morloking <dyubenkosergei@yandex.ru>
2026-03-25 19:53:26 +0300

    Update parallelogram.cpp

--- parallelogram.cpp
+++ parallelogram.cpp
@@ -1,4 +1,4 @@
 #include "parallelogram.h"
 
 my_namespace::Parallelogram::Parallelogram(int l1, int l2, int c1, int c2) :
-	Quadrangle(l1, l2, l1, l2, c1, c2, c1, c2, "Параллелограмм") {}
+	Quadrangle(l1, l2, l1, l2, c1, c2, c1, c2, "РџР°СЂР°Р»Р»РµР»РѕРіСЂР°РјРј") {}

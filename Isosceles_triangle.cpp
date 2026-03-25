19347134d9aae12f829d1d5febbe8e94c86306c1
Morloking <dyubenkosergei@yandex.ru>
2026-03-25 19:55:39 +0300

    Update Isosceles_triangle.cpp

--- Isosceles_triangle.cpp
+++ Isosceles_triangle.cpp
@@ -1,4 +1,4 @@
 #include "isosceles_triangle.h"
 
 my_namespace::IsoscelesTriangle::IsoscelesTriangle(int l1, int l2, int c1, int c2) :
-	Triangle(l1, l2, l1, c1, c2, c1, "Равнобедренный треугольник") {}
+	Triangle(l1, l2, l1, c1, c2, c1, "Р Р°РІРЅРѕР±РµРґСЂРµРЅРЅС‹Р№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє") {}

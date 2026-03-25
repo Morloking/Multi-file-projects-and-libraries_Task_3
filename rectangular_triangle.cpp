68db509be99c0415e5f4637dc833dcacdb1835b3
Morloking <dyubenkosergei@yandex.ru>
2026-03-25 19:52:32 +0300

    Update rectangular_triangle.cpp

--- rectangular_triangle.cpp
+++ rectangular_triangle.cpp
@@ -1,4 +1,4 @@
 #include "rectangular_triangle.h"
 
 my_namespace::RectangularTriangle::RectangularTriangle(int l1, int l2, int l3, int c1, int c2) :
-	Triangle(l1, l2, l3, c1, c2, 90, "Прямоугольный треугольник") {}
+	Triangle(l1, l2, l3, c1, c2, 90, "РџСЂСЏРјРѕСѓРіРѕР»СЊРЅС‹Р№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє") {}
